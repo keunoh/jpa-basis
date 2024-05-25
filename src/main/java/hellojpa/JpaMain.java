ddca28b8a92177039929b73541495771ceabc495
kaltz <rmsdhcjswo1@gmail.com>
2024-05-26 00:50:25 +0900

    JPA persist && update

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -6,11 +6,29 @@ public class JpaMain {
 
     public static void main(String[] args) {
 
+        // 이때 DB도 연결된다. hello 라는 엔티티 매니저 팩토리를 생성한다.
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        //code
+        // 여기서 이제 실제 DB CRUD 작성한다.
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        // 끝
         emf.close();
     }
 }
